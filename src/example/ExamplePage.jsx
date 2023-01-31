89a5a268f19b5be5fe30f4ffd4d4507acc242476
Adam Stankiewicz <agstanki@gmail.com>
2023-01-31 07:57:42 -0500

    Initial commit

---
+++ src/example/ExamplePage.jsx
@@ -0,0 +1,10 @@
+const ExamplePage = () => {
+  <main>
+    <div className="container-fluid">
+      <h1>Example Page</h1>
+      <p>Hello world!</p>
+    </div>
+  </main>;
+};
+
+export default ExamplePage;
